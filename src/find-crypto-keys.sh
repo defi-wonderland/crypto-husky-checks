9a514571b5f25b77b412bf37143f5adbb6d9eac0
turtlemoji <turtlemoji@protonmail.com>
2024-05-16 20:02:54 -0300

    fix: multiple leaks and tests

--- src/find-crypto-keys.sh
+++ src/find-crypto-keys.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 RESTORE='\033[0m'
 RED='\033[00;31m'
@@ -10,10 +10,10 @@ FOUND=""
 # Check HEAD
 if git rev-parse --verify HEAD > /dev/null 2>&1
 then
-	AGAINST=HEAD
+    AGAINST=HEAD
 else
-    empty_tree=$( git hash-object -t tree /dev/null )
-	AGAINST="$empty_tree"
+    empty_tree=$(git hash-object -t tree /dev/null)
+    AGAINST="$empty_tree"
 fi
 
 # Get a list of files in the index excluding deleted files
@@ -21,19 +21,18 @@ FILE_LIST=$(git diff --cached --name-only --diff-filter=d ${AGAINST})
 SHOW_FIRST=5
 SHOW_LAST=10
 ASTERISKS_LENGTH=$(expr 64 - ${SHOW_FIRST} - ${SHOW_LAST})
-ASTERISKS=$(printf "*%.0s" $(seq $ASTERISKS_LENGTH))
+ASTERISKS=$(printf "%0.s*" $(seq $ASTERISKS_LENGTH))
 
 # For each file with changes
 for FILE in $FILE_LIST
 do
-    if [ ! -z $(git diff --cached --unified=0 $FILE | grep '^+' | grep -E -o '[1234567890abcdefABCDEF]{64}') ]; then
-        QUERY=`git diff --cached $FILE | grep '^+' | grep -E -o '[1234567890abcdefABCDEF]{64}' | sed -r "s/(.{${SHOW_FIRST}})(.*)(.{${SHOW_LAST}})/\1$ASTERISKS\3/"`
+    if [ ! -z "$(git diff --cached --unified=0 $FILE | grep '^+' | grep -E -o '[1234567890abcdefABCDEF]{64}')" ]; then
+        QUERY=$(git diff --cached $FILE | grep '^+' | grep -E -o '[1234567890abcdefABCDEF]{64}' | sed -r "s/(.{${SHOW_FIRST}})(.*)(.{${SHOW_LAST}})/\1$ASTERISKS\3/")
         FOUND="${FOUND} ${BLUE}${FILE}:${RED}${QUERY}${RESTORE}\n"
     fi
 done
 
 # if FOUND is not empty, REJECT the COMMIT
-
 if [ ! -z "$FOUND" ]; then
     printf "${RED}COMMIT REJECTED: ${RESTORE}"
     printf "${YELLOW}Possible private key${RESTORE}\n"
--- test/1_find-crypto-keys.sh
+++ test/1_find-crypto-keys.sh
@@ -3,31 +3,84 @@
 setup
 install
 
+####################################################
 # Expect to copy file to husky folder
+####################################################
 SCRIPTS_PATH='.husky/wonderland';
 expect 1 "[ ! -f ${SCRIPTS_PATH}/find-crypto-keys.sh ] && exit 2"
 ok "Expect script correctly copied"
 
+####################################################
 # Expect script to be added on pre-commit file
+####################################################
 TEXT="'. \"\$(dirname \"\$0\")/wonderland/find-crypto-keys.sh\"'"
 expect 0 "grep -q ${TEXT} .husky/pre-commit"
 ok "Expect script on precommit file"
 
+####################################################
 # Expect commit to pass
+####################################################
 git add .
 expect 0 "git commit -m foo -q"
 ok "Expect commit to pass"
 
+####################################################
 # Expect commit with leak to fail
+####################################################
 cat <<EOF >testfile
 first line
 testLeak=abd3d28282b3b0e68918328527dbf73f6e79781bbaded0b24fdc3e43bf6fc56e
-third line
+end line
 EOF
 
 git add .
 expect 1 "git commit -m foo -q > /dev/null 2>&1"
-ok "Expect commit to fail"
+ok "Expect commit with leaks to fail"
+
+####################################################
+# Expect commit fixed to pass
+####################################################
+cat <<EOF >testfile
+first line
+testLeak=fixed
+end line
+EOF
+
+git add .
+expect 0 "git commit -m foo -q"
+ok "Expect commit fixed to pass"
+
+####################################################
+# Expect commit with multiple leaks to fail
+####################################################
+cat <<EOF >testfile
+first line
+testLeak1=abd3d28282b3b0e68918328527dbf73f6e79781bbaded0b24fdc3e43bf6fc561
+testLeak2=abd3d28282b3b0e68918328527dbf73f6e79781bbaded0b24fdc3e43bf6fc562
+testLeak3=abd3d28282b3b0e68918328527dbf73f6e79781bbaded0b24fdc3e43bf6fc563
+end line
+EOF
+
+git add .
+expect 1 "git commit -m foo -q > /dev/null 2>&1"
+ok "Expect commit with multiple leaks to fail"
+
+####################################################
+# Expect commit with multiple files and leaks to fail
+####################################################
+cat <<EOF >testfile2
+first line
+testLeak4=abd3d28282b3b0e68918328527dbf73f6e79781bbaded0b24fdc3e43bf6fc564
+testLeak5=abd3d28282b3b0e68918328527dbf73f6e79781bbaded0b24fdc3e43bf6fc565
+testLeak6=abd3d28282b3b0e68918328527dbf73f6e79781bbaded0b24fdc3e43bf6fc566
+end line
+EOF
+
+git add .
+expect 1 "git commit -m foo -q > /dev/null 2>&1"
+ok "Expect commit with multiple files and leaks to fail"
+
+####################################################
 
 # Clean test folder
 clean
