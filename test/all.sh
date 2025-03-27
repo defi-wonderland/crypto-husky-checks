f2b469412a85b1670acce40f2eb6f8d77b35b9ec
turtlemoji <turtlemoji@protonmail.com>
2025-03-27 01:56:36 -0300

    fix: pnpm

--- test/all.sh
+++ test/all.sh
@@ -1,10 +1,19 @@
 set -e
 # Create package and move it to tmp
 npm pack && mv defi-wonderland-crypto-husky-checks-*.tgz /tmp/crypto-husky-checks.tgz
+npm install -g pnpm
 
 ####### Tests
 
+echo "Running tests with npm..."
 sh test/1_find-crypto-keys.sh
+
+echo "Running tests with pnpm..."
+export PACKAGE_MANAGER=pnpm
+
+# Try to run the test with pnpm
+sh test/1_find-crypto-keys.sh
+unset PACKAGE_MANAGER
 
 ####### End Tests
 
